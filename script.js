d82a4d786cbca92ab498efc50f62bfa3658ed512
MatheusAiala <matheussurg10@li.gmail.com>
2025-07-12 14:49:20 -0300

    arrumei a posição dos itens

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const searchInput = document.getElementById('seach');
+const seachButton = document.getElementById('seach-button');
+
+consttableBody = minhatale ? minhatale.querySelector('tbody') : null;
+
+const initialTableRows = tableBody ? Array.from(tableBody.querySelectorAll('tr')) : [];
+
 const button = document.getElementById('toggleButton');
 const table = document.getElementById('minha_table');
 
@@ -12,10 +21,37 @@ button.textContent = '+';
 }
 
 });
+});
 
-const searchInput = document.getElementById('seach');
-const seachButton = document.getElementById('seach-button');
+function performSearch(){
+    const seachTerm = seachInput.value.toLowerCase().trim()
 
-consttableBody = minhatale ? minhatale.querySelector('tbody') : null;
+    tableRows.forEach(roe =>{
+        let rowMatches = false;
+        const cells = row.querySelectorAll('td');
 
-const initialTableRows = tableBody ? Array.from(tableBody.querySelectorAll('tr')) : [];
+        cells.forEach(cell => {
+            if (cell.textContent.toLowerCase().includes(seachTerm)) {
+                rowMatches = true;
+            }
+        });
+
+        if (rowMatchs) {
+            row.style.display = '';
+        }else {
+            row.style.display = 'none'
+        }
+    });
+}
+
+if (seachButton) {
+    searchButtom.addEventListener('click', performSearch); 
+}
+
+if (searchInput) {
+    searchInputaddEventListener('keypress', function(event) {
+        if (event.key === "enter") {
+            performSearch();
+        }
+    });
+}
